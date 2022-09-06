2039f4c6494beef0eea18a72f69f7dc21c96b0e7
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-06 12:25:18 -0700

    Add user story extension 2 add contestant to project test not finished

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -62,4 +62,31 @@ RSpec.describe 'project show' do
         expect(page).to have_content("Average Contestant Experience: 12.5 years")
         save_and_open_page
     end
+
+    xit 'can add a contestant to a project' do
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+        erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+        ContestantProject.create(contestant_id: jay.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: kentaro.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: kentaro.id, project_id: @boardfit.id)
+        ContestantProject.create(contestant_id: erin.id, project_id: @boardfit.id)
+        
+        visit "/projects/#{@news_chic.id}"
+
+        fill_in("Search", with: "#{kentaro.name}")
+        click_on("Submit")
+        within("#contestant_#{kentaro.id}") do
+            click_on("Add contestant to Project")
+        end
+        expect(current_path).to eq("/projects/#{@news_chic.id}")
+        expect(page).to have_content("Number of Contestants: 3")
+        visit "/contestants"
+        #within for kentaro projects?
+    end
 end
