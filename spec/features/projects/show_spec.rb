7f426456eb08c01ebb68a62f2e1471963af76602
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-06 11:56:40 -0700

    User story extension 1 feature test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -40,6 +40,25 @@ RSpec.describe 'project show' do
         expect(page).to have_content(@news_chic.name)
         expect(page).to have_content(@news_chic.enrollment)
         
-       save_and_open_page
+       #save_and_open_page
+    end
+
+    it 'shows the average years of xp of the contestants' do
+        jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+        kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+        erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+        ContestantProject.create(contestant_id: jay.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: gretchen.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: kentaro.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: kentaro.id, project_id: @boardfit.id)
+        ContestantProject.create(contestant_id: erin.id, project_id: @boardfit.id)
+        
+        visit "/projects/#{@news_chic.id}"
+
+        expect(page).to have_content("Average Contestant Experience: 38 years")
     end
 end
