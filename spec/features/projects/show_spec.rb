c386de276f3773aa9edc533cc469892f7ed52752
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-06 10:27:08 -0700

    Add user story 1 project show functionality

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe 'project show' do
         @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
         @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-        @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-        @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+        @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-        @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-        @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+        @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
     end
 
     it "shows project attributes" do
         visit "/projects/#{@lit_fit.id}"
         expect(page).to have_content(@lit_fit.name)
-        expect(page).to have_content("Material: Lampshade")
+        expect(page).to have_content("Material: Lamp")
         expect(page).to have_content("Challenge Theme: Apartment Furnishings")
-        #save_and_open_page
+        save_and_open_page
     end
 end
